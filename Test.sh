5ce3b60f431024ff216ac51237e78edb5a5ea04e
wesleyriansmith <W.Smith7@newcastle.ac.uk>
2024-05-03 13:15:48 +0100

    Updated bash file

--- Test.sh
+++ Test.sh
@@ -2,7 +2,7 @@
 # Load necessary modules
 module load python/anaconda3
 source $condaDotFile
-source activate gymnasium
+source activate RLProject
 
 # Display GPU status
 nvidia-smi
@@ -10,16 +10,8 @@ nvidia-smi
 # Display the Python interpreter location
 which python
 
-# to tmp ray
-mkdir -p /jmain02/home/J2AD006/jxb06/cxz46-jxb06//ray/$SLURM_JOB_ID
-ln -sf /jmain02/home/J2AD006/jxb06/cxz46-jxb06//ray/$SLURM_JOB_ID /tmp/ray_$SLURM_JOB_ID
-
 # Define paths
-_path="/jmain02/home/Disseration210168637"
-
-
-echo "Job_ID: $SLURM_JOB_ID, Task: atari_dpber_ram_saver.py, Env: Atlantis" >> \
-/jmain02/home/J2AD006/jxb06/cxz46-jxb06//run_log_atari/jobs_mapping.log
+_path="C:\Users\smith\PycharmProjects\Diss"
 
 # Change directory to the script's path
 cd $_path || exit
