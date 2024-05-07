d31e146e56e938780e79cbda1c0aeef7ee482872
WesleyRianSmith <w.smith7@newcastle.ac.uk>
2024-05-07 19:04:47 +0100

    Pushed fresh bash file

--- BashScripts/Test.sh
+++ BashScripts/Test.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 # Load necessary modules
-module load python/anaconda3
-source $condaDotFile
-source activate csc1016863
-
-# Display GPU status
-nvidia-smi
-
-# Display the Python interpreter location
-which python
-
-# Define paths
-_path="/jmain02/home/J2AD006/jxb06/cxz46-jxb06/csc210168637/RLExperiments"
+_path="/home/wesleyubuntu/RLExperiments"
 
 # Change directory to the script's path
 cd $_path || exit
 
 # Execute the Python script with parameters
-python $_path/PPOExperiment.py -C $_path/Configurations/PPO/BreakoutDefault.yml
+python3 $_path/PPOExperiment.py -C $_path/Configurations/PPO/BreakoutDefault.yml
 
