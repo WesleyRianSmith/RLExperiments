d359eaa533e7625e78ffc287ce59cd0147cd09a1
wesleyriansmith <W.Smith7@newcastle.ac.uk>
2024-05-07 20:35:11 +0100

    Added bash script and configuration files

--- BashCreator.py
+++ BashCreator.py
@@ -1,5 +1,5 @@
 path = "/jmain02/home/J2AD006/jxb06/cxz46-jxb06/csc210168637/RLExperiments"
-def write_bash_script(bash_name,experiment_script, configure_file):
+def write_bash_script(bash_name,experiment_script,architecture, configure_file):
     
 	
     bash_content = f"""#!/bin/bash
@@ -13,7 +13,7 @@ which python
 
 _path="{path}"
 cd $_path || exit
-python $_path/{experiment_script}.py -C $_path/Configurations/PPO/{configure_file}.yml
+python $_path/{experiment_script}.py -C $_path/Configurations/{architecture}/{configure_file}.yml
 """
     # Write the bash script to a file
     with open(f'BashScripts/{bash_name}.sh', 'w') as file:
@@ -22,4 +22,30 @@ python $_path/{experiment_script}.py -C $_path/Configurations/PPO/{configure_fil
     print("Bash script 'run_experiment.sh' has been created.")
 
 if __name__ == '__main__':
-    write_bash_script("BreakoutDefault","PPOExperiment","BreakoutDefault")
+    write_bash_script("PongPPO_Default","PPOExperiment","PPO","PongPPO_Default")
+    write_bash_script("PongPPO_Tuned", "PPOExperiment","PPO", "PongPPO_Tuned")
+    write_bash_script("PongDQN_Default", "DQNExperiment","DQN", "PongDQN_Default")
+    write_bash_script("PongDQN_Tuned", "DQNExperiment","DQN", "PongDQN_Tuned")
+    write_bash_script("PongA2C_Default", "A2CExperiment","A2C", "PongA2C_Default")
+    write_bash_script("PongA2C_Tuned", "A2CExperiment","A2C", "PongA2C_Tuned")
+
+    write_bash_script("BreakoutPPO_Default", "PPOExperiment","PPO", "BreakoutPPO_Default")
+    write_bash_script("BreakoutPPO_Tuned", "PPOExperiment","PPO", "BreakoutPPO_Tuned")
+    write_bash_script("BreakoutDQN_Default", "DQNExperiment","DQN", "BreakoutDQN_Default")
+    write_bash_script("BreakoutDQN_Tuned", "DQNExperiment","DQN", "BreakoutDQN_Tuned")
+    write_bash_script("BreakoutA2C_Default", "A2CExperiment","A2C", "BreakoutA2C_Default")
+    write_bash_script("BreakoutA2C_Tuned", "A2CExperiment","A2C", "BreakoutA2C_Tuned")
+
+    write_bash_script("SpaceInvadersPPO_Default", "PPOExperiment","PPO", "SpaceInvadersPPO_Default")
+    write_bash_script("SpaceInvadersPPO_Tuned", "PPOExperiment","PPO", "SpaceInvadersPPO_Tuned")
+    write_bash_script("SpaceInvadersDQN_Default", "DQNExperiment","DQN", "SpaceInvadersDQN_Default")
+    write_bash_script("SpaceInvadersDQN_Tuned", "DQNExperiment","DQN", "SpaceInvadersDQN_Tuned")
+    write_bash_script("SpaceInvadersA2C_Default", "A2CExperiment","A2C", "SpaceInvadersA2C_Default")
+    write_bash_script("SpaceInvadersA2C_Tuned", "A2CExperiment","A2C", "SpaceInvadersA2C_Tuned")
+
+    write_bash_script("PacManPPO_Default", "PPOExperiment","PPO", "PacManPPO_Default")
+    write_bash_script("PacManPPO_Tuned", "PPOExperiment","PPO", "PacManPPO_Tuned")
+    write_bash_script("PacManDQN_Default", "DQNExperiment","DQN", "PacManDQN_Default")
+    write_bash_script("PacManDQN_Tuned", "DQNExperiment","DQN", "PacManDQN_Tuned")
+    write_bash_script("PacManA2C_Default", "A2CExperiment","A2C", "PacManA2C_Default")
+    write_bash_script("PacManA2C_Tuned", "A2CExperiment","A2C", "PacManA2C_Tuned")
---
+++ DQNExperiment.py
@@ -0,0 +1,44 @@
+import yaml
+import experiment
+import os
+from stable_baselines3 import DQN
+from stable_baselines3.common.vec_env import VecFrameStack
+from stable_baselines3.common.env_util import make_atari_env
+
+def load_config(path):
+    with open(path, 'r') as file:
+        return yaml.safe_load(file)
+
+
+def main(config):
+    directory = f"ExperimentModels"
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    experiment_name = config["experiment_name"]
+    env_id = config["env_id"]
+    n_envs = config["n_envs"]
+    n_stack = config["n_stack"]
+    eval_frequency = config["eval_frequency"]
+
+    env = make_atari_env(env_id, n_envs=n_envs, seed=0)
+    env = VecFrameStack(env, n_stack=n_stack)
+    hyperparameters = config["hyperparameters"]
+
+    model = DQN(env=env, **hyperparameters)
+    experiment.InitialiseExperiment(
+        experiment_name=experiment_name, model=model, model_architecture="PPO",
+        env_id=env_id, n_envs=n_envs, n_stack=n_stack, eval_frequency=eval_frequency,
+        hyper_parameters=hyperparameters
+    )
+
+
+    experiment.TrainExperiment(experiment_name, config["steps_to_train"])
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='Run PPO experiments with configurations from a YAML file.')
+    parser.add_argument('-C', '--config', type=str, required=True, help='Path to configuration YAML file')
+    args = parser.parse_args()
+
+    config = load_config(args.config)
+    main(config)
